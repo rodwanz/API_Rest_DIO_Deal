2bb17c6f1c16fa13af5ca552964a772a2c8050e0
rodwanz <wanzervai@gmail.com>
2024-09-24 07:49:02 -0300

    Iniciando o projeto

---
+++ src/test/java/me/dio/desafio/deal/DesafioDealApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafio.deal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
