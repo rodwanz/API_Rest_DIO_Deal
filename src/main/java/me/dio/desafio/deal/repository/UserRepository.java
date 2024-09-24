f0bf7e4f7d786235e06ca535e4fcddb42e4048ab
rodwanz <wanzervai@gmail.com>
2024-09-24 11:40:46 -0300

    Deixando tudo ajustado

---
+++ src/main/java/me/dio/desafio/deal/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.desafio.deal.repository;
+
+import me.dio.desafio.deal.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
